80aed323ee8c57460804df0c7f4daf3841ebac46
Sinan Altuntas <sinanaltuntas.ist@gmail.com>
2020-09-30 23:40:48 +0300

    updated file

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,6 +3,9 @@ public class Hello {
     public static void main(String[] args) {
 
 
-        System.out.println("hello");
+        System.out.println("hello class EU3");
+
+        // more changes are coming
+        //i am ready to commit this
     }
 }
